89d706fd8901a434b8bbbd23a845ad76a9513ca5
Bao Le <bao29102002@gmail.com>
2023-12-04 15:31:04 +0700

    update

--- script/script.js
+++ script/script.js
@@ -1,32 +1,40 @@
-let slideIndex = 1;
 let timeout
-var slides = document.getElementsByClassName("slide-item");
-var dots = document.getElementsByClassName("dot");
-showSlides(slideIndex)
+const slides = document.getElementsByClassName("slide-item");
+const dots = document.getElementsByClassName("dot");
+showSlides(1)
+const curentSlideIndex = () => {
+  let currentIndex = 0
+  Array.from(slides).map((slide, index) => {
+    if (slide.style.display == 'block') {
+      currentIndex = index
+    }
+  })
+  return currentIndex + 1
+}
 function plusSlides(n) {
   clearTimeout(timeout)
-  slideIndex += (n - 1)
-  showSlides(slideIndex);
+  showSlides(curentSlideIndex() + n);
 }
 function setCurrentSlide(n) {
   clearTimeout(timeout)
-  slideIndex = n
-  showSlides(slideIndex)
+  showSlides(n)
 }
-function showSlides(n) {
+function showSlides(slideIndex) {
   let i;
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"
   }
   if (slideIndex > slides.length) {
     slideIndex = 1
   }
-  if (n < 1) { slideIndex = slides.length }
+  if (slideIndex < 1) { slideIndex = slides.length }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "")
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  slideIndex++
-  timeout = setTimeout(showSlides, 5000);
+  timeout = setTimeout(autoNextSlide, 5000)
+}
+function autoNextSlide() {
+  showSlides(curentSlideIndex() + 1)
 }
